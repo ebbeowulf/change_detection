453467e08f6721d8f19143b7427a6ca100e34ff5
Eric Martinson <emartinso@ltu.edu>
2024-01-11 16:02:10 +0000

    generate nerfstudio images

--- bash/generate_nerfstudio_image.sh
+++ bash/generate_nerfstudio_image.sh
@@ -2,7 +2,7 @@
 
 BASE_NERFSTUDIO_DIR="/home/emartinso/projects/nerfstudio/"
 CONFIG_DIR="outputs/nerf_no_person_initial/nerfacto/2024-01-04_035029/"
-SCRIPTS_DIR=$(pwd)+"../scripts/"
+SCRIPTS_DIR="$(pwd)/../scripts/"
 
 TARGET_DIR=$1
 RENDER_SAVE_DIR=$2
@@ -20,10 +20,14 @@ cd $BASE_NERFSTUDIO_DIR
 transforms=$TARGET_DIR/transforms.json
 if [[ ! -f $transforms ]];then
     python $SCRIPTS_DIR/colmap_to_json.py $TARGET_DIR/colmap_combined/sparse_geo $TARGET_DIR
+fi
 
 # Have we created the nerf images?
 rgb_fName="rgb_$IMAGE_NUM.png"
 if [[ ! -f $RENDER_SAVE_DIR/$rgb_fName ]];then
-    python $SCRIPTS_DIR/render_transform.py $CONFIG_DIR $transforms $RENDER_SAVE_DIR --name-filter $IMAGE_NUM --image-type all
+    if [[ ! -d $RENDER_SAVE_DIR ]];then
+        mkdir -p $RENDER_SAVE_DIR
+    fi
+    python $SCRIPTS_DIR/render_transform.py $CONFIG_DIR $transforms $RENDER_SAVE_DIR --name-filter $rgb_fName --image-type all
 fi
 
